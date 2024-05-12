675c5c03e2148411b9e102e5432ff15a578dc841
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-05-12 09:30:30 +0530

    folder

--- Node_learning/node_coremodules/index.js
+++
@@ -1,9 +0,0 @@
-const a="piu";
-console.log(a);
-const fs=require("fs");
-fs.writeFileSync("read.txt","Hello node");
-fs.appendFileSync("read.txt","Hii ,learning node js");
-const buf_data=fs.readFileSync('read.txt');
-console.log(buf_data);
-console.log(buf_data.toString());
-fs.renameSync('read.txt','readwrite.txt');
