1cedc7856c86eaf05313f95a9ed6c82405d7de2e
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-05-13 19:12:10 +0530

    Routing

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,24 @@
 const http = require('http');
 const server = http.createServer((req,res)=>
     {
-        res.end("This is the response of server");
+        if(req.url== '/')
+            {
+                res.end("This is the response of server");
 
+            }
+        else if(req.url=='/about')
+            {
+                res.end("about");
+            }
+        else if(req.url=='/home')
+            {
+                res.end("Inside Home");
+            }
+        else
+        {
+            res.end("404 error page");
+        }
+        
     });
 server.listen(8000,"127.0.0.1",()=>
 {
